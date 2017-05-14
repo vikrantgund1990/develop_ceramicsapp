1ad0677e98ca5084f8577ed3959224852b722472
Prakash <prakash.kumare@loylty.com>
2017-05-14 20:13:48 +0530

    add 4 option add social media icon add visualizer option on sliding images add product fragment

--- app/src/main/java/ceramics/com/ceramics/fragments/BaseFragment.java
+++ app/src/main/java/ceramics/com/ceramics/fragments/BaseFragment.java
@@ -1,13 +1,10 @@
 package ceramics.com.ceramics.fragments;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 
 /**
- * Created by vikrantg on 25-02-2017.
+ * Created by Prakash on 5/14/2017.
  */
 
 public class BaseFragment extends Fragment {
-
-
 }
