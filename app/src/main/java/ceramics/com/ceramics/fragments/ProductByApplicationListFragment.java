e2d0242bdc3f7b086b605081743d1b89892b7eaf
vikrantg <vikrant.gund@loylty.com>
2017-05-22 20:28:15 +0530

    add product by application list

--- app/src/main/java/ceramics/com/ceramics/fragments/ProductByApplicationListFragment.java
+++ app/src/main/java/ceramics/com/ceramics/fragments/ProductByApplicationListFragment.java
@@ -5,8 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,6 +24,7 @@ import java.util.ArrayList;
 import ceramics.com.ceramics.R;
 import ceramics.com.ceramics.activity.BaseActivity;
 import ceramics.com.ceramics.adapter.ProductListGridAdapater;
+import ceramics.com.ceramics.custom.ListPopupWindow;
 import ceramics.com.ceramics.model.ProductDetails;
 import ceramics.com.ceramics.network.APIRequestHelper;
 import ceramics.com.ceramics.network.CommonJsonArrayModel;
@@ -36,8 +39,12 @@ public class ProductByApplicationListFragment extends BaseFragment implements Vi
 
     private BaseActivity activity;
     private GridView gvProductList;
+    private TextView tvSize;
     private ProductListGridAdapater productListGridAdapater;
     private ArrayList<ProductDetails> productList;
+    private ArrayAdapter<String> sizeAdapter;
+    private ListPopupWindow lpwSize;
+    private ArrayList<String> sizeList;
 
     @Nullable
     @Override
@@ -61,16 +68,33 @@ public class ProductByApplicationListFragment extends BaseFragment implements Vi
 
     private void initView(View view){
         gvProductList = (GridView)view.findViewById(R.id.grid_products);
+        tvSize = (TextView)view.findViewById(R.id.filter_size);
         Bundle bundle = getArguments();
         productList = (ArrayList<ProductDetails>) bundle.getSerializable("ProductList");
         productListGridAdapater = new ProductListGridAdapater(activity,productList);
         gvProductList.setAdapter(productListGridAdapater);
+
+        sizeList = new ArrayList<>();
+        sizeList.add("30 X 20");
+        sizeList.add("10 X 30");
+        sizeList.add("20 X 10");
+        sizeList.add("40 X 50");
+
+        sizeAdapter = new ArrayAdapter<String>(activity,R.layout.row_dropdown_list,sizeList);
+        lpwSize = new ListPopupWindow(activity);
+        lpwSize.setAdapter(sizeAdapter);
+        lpwSize.setAnchorView(tvSize);
+        lpwSize.setModal(true);
+        lpwSize.setPromptPosition(ListPopupWindow.POSITION_PROMPT_BELOW);
+        tvSize.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (Utils.isNetworkConnected(activity)){
             switch (v.getId()){
+                case R.id.filter_size:
+                    lpwSize.show();
             }
         }
         else {
