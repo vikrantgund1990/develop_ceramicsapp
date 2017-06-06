2f9ada7b6b6632eaa575b3d76547245690d3c565
vikrantg <vikrant.gund@loylty.com>
2017-06-06 11:33:39 +0530

    add visualizer URL

--- app/src/main/java/ceramics/com/ceramics/adapter/ImageListAdapter.java
+++ app/src/main/java/ceramics/com/ceramics/adapter/ImageListAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
@@ -47,6 +48,7 @@ public class ImageListAdapter extends ArrayAdapter<String> {
             holder = new ViewHolder();
             convertView = inflater.inflate(R.layout.row_image_lsit,null);
             holder.ivImage = (NetworkImageView)convertView.findViewById(R.id.image_product);
+            holder.tvName = (TextView)convertView.findViewById(R.id.text_name);
             convertView.setTag(holder);
         }
         else {
@@ -54,11 +56,32 @@ public class ImageListAdapter extends ArrayAdapter<String> {
         }
 
         holder.ivImage.setImageUrl(imageList.get(position),imageLoader);
-
+        holder.tvName.setText(getApplicationName(position));
         return convertView;
+    }
+
+    private String getApplicationName(int position){
+        String name = "";
+        switch (position){
+            case 0: name = "Bedroom";
+                    break;
+            case 1: name = "Living Room";
+                    break;
+            case 2: name = "Kitchen";
+                    break;
+            case 3: name = "Bathroom";
+                    break;
+            case 4: name = "Office";
+                    break;
+            case 5: name = "Outdoor";
+                    break;
+        }
+
+        return name;
     }
 
     class ViewHolder{
         private NetworkImageView ivImage;
+        private TextView tvName;
     }
 }
